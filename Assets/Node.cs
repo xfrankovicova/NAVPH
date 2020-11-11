df58a0b3382974cdad40b29ffd488ff7e8043032
adler_cvikerar <ckr.adler@gmail.com>
2020-11-11 12:57:32 +0100

    rozdrbane

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -22,39 +22,5 @@ public class Node {
 			);
 	}
 
-	public List<Tuple<int, int>> GetNeighboursIndexes() 
-	{
-        List<Tuple<int, int>> neighbours = new List<Tuple<int, int>>();
-        List<Tuple<int, int>> result = new List<Tuple<int, int>>();
-        //get left
-        neighbours.Add(new Tuple<int, int>(x - 1, y));
-        //get right 
-        neighbours.Add(new Tuple<int, int>(x + 1, y));
-        //get down 
-        neighbours.Add(new Tuple<int, int>(x, y - 1));
-        //get up 
-        neighbours.Add(new Tuple<int, int>(x, y + 1));
-        //if even
-        if (x % 2 == 0)
-        {
-            neighbours.Add(new Tuple<int, int>(x - 1, y + 1));
-            neighbours.Add(new Tuple<int, int>(x + 1, y + 1));
-        }
-        else
-        {
-            neighbours.Add(new Tuple<int, int>(x - 1, y - 1));
-            neighbours.Add(new Tuple<int, int>(x + 1, y - 1));
-        }
-
-        foreach (var item in neighbours)
-        {
-            if (!(item.Item1 < 0 || item.Item1 >= Constants.gridSizeX || item.Item2 < 0 || item.Item2 >= Constants.gridSizeY))
-            {
-                result.Add(item);
-            }
-        }
-
-        return result;
-    }
 
 }
